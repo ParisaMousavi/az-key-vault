4e380a6cf5fb08858cd25c3ae7e6506aa3d8e936
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-10-25 20:48:36 +0200

    update

---
+++ input.tf
@@ -0,0 +1,65 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "soft_delete_retention_days" {
+  type    = number
+  default = 60
+}
+
+variable "purge_protection" {
+  type    = bool
+  default = false
+}
+
+variable "enabled_for_disk_encryption" {
+  type    = bool
+  default = false
+}
+
+variable "enabled_for_deployment" {
+  type    = bool
+  default = false
+}
+
+variable "enabled_for_template_deployment" {
+  type    = bool
+  default = false
+}
+
+variable "enable_rbac_authorization" {
+  type    = bool
+  default = false
+}
+
+variable "sku_name" {
+  type    = string
+  default = "standard"
+}
+
+variable "additional_tags" {
+  default = {}
+  type    = map(string)
+}
+
+variable "network_acls" {
+  type = object({
+    bypass                     = string
+    default_action             = string
+    ip_rules                   = list(string)
+    virtual_network_subnet_ids = list(string)
+  })
+}
+
---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_key_vault" "this" {
+  name                            = var.name
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  tenant_id                       = var.tenant_id
+  soft_delete_retention_days      = var.stage == "prod" || var.stage == "acc" ? 60 : var.soft_delete_retention_days
+  purge_protection_enabled        = var.stage == "prod" || var.stage == "acc" ? true : var.purge_protection
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  enable_rbac_authorization       = var.enable_rbac_authorization
+  sku_name                        = "standard"
+  dynamic "network_acls" {
+    for_each = var.network_acls.bypass != null ? [0] : []
+    content {
+      bypass                     = var.network_acls.bypass
+      default_action             = var.network_acls.default_action
+      ip_rules                   = var.network_acls.ip_rules
+      virtual_network_subnet_ids = var.network_acls.virtual_network_subnet_ids
+    }
+  }
+  tags = merge(
+    var.additional_tags,
+    {
+      created-by = "iac-tf"
+    },
+  )
+}
