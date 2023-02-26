fa86b2dabe2c568620c6fc1b0254cf00d8179e2b
Adatum User1 <user1@adatum.com>
2023-02-26 10:08:38 +0100

    update

--- output.tf
+++ output.tf
@@ -17,3 +17,7 @@ output "vault_uri" {
 output "network_interface_id" {
   value = var.private_endpoint_config.subnet_id == null ? null : azurerm_private_endpoint.this[0].network_interface.0.id
 }
+
+output "custom_dns_configs" {
+  value = var.private_endpoint_config.subnet_id == null ? null : azurerm_private_endpoint.this[0].custom_dns_configs
+}
